0f165066a2b3afb2b027d3e165cdcf0bed27422b
dineshdhakal <dineshbdhakal@gmail.com>
2015-07-18 15:06:59 +0530

    The Beginning!

---
+++ app/src/main/java/net/dineshd/sewa/Cust_Log_inFragment.java
@@ -0,0 +1,23 @@
+package net.dineshd.sewa;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Cust_Log_inFragment extends Fragment {
+
+    public Cust_Log_inFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cust__log_in, container, false);
+    }
+}
